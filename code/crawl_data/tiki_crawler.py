9626d325d6466c9162b612910d44058052ea3255
vukhanh09 <vukhanh0920@gmail.com>
2022-10-21 16:48:09 +0000

    crawl review

--- code/crawl_data/tiki_crawler.py
+++ code/crawl_data/tiki_crawler.py
@@ -14,12 +14,15 @@ class Tiki:
         
         self.api_get_id_product = 'https://tiki.vn/api/personalish/v1/blocks/listings?limit=40&include=advertisement&aggregations=2&trackity_id=98ad34b4-6944-2947-2f4a-ff17de7e2a42&category={}&page={}&urlKey={}'
         
-        self.api_product = 'https://tiki.vn/api/v2/products/{}'
+        self.api_product = 'https://tiki.vn/api/v2/products/{}?platform=web&spid={}'
         
         self.headers={
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                           "Chrome/100.0.4896.127 Safari/537.36 "
         }
+        
+        
+        self.api_review = 'https://tiki.vn/api/v2/reviews?limit=20&include=comments,contribute_info,attribute_vote_summary&sort=score%7Cdesc,id%7Cdesc,stars%7Call&page={}&spid={}&product_id={}'
         
         self.get_category()
         
@@ -41,7 +44,7 @@ class Tiki:
         if response.status_code == 200:
             data = json.loads(response.text)['data']
             for sample in data:
-                list_ids.append(sample['id'])
+                list_ids.append([sample['id'],sample['seller_product_id']])
 
         return list_ids
 
@@ -50,5 +53,19 @@ class Tiki:
         if response.status_code == 200:
             data = json.loads(response.text)
             return data
+        
+    def get_max_paging_review(self,url):
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data['paging']['last_page']
+        return 0
+    
+    def get_review(self,url):
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data
+        
     
     
