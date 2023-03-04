25935fd8e17062f460730ad94ffebea0a6c781bf
vukhanh09 <vukhanh0920@gmail.com>
2023-03-04 13:30:31 +0000

    add

---
+++ code/demo/.ipynb_checkpoints/product-consumer-checkpoint.py
@@ -0,0 +1,76 @@
+import json
+import re
+from tqdm import tqdm
+import pandas as pd
+import time
+import warnings
+warnings.filterwarnings("ignore")
+import argparse
+
+from json import dumps
+from kafka import KafkaConsumer
+from json import loads
+from pyspark.sql import SparkSession
+import os
+
+# define environment kafka streaming
+kafka = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0"
+spark = "org.apache.spark:spark-streaming-kafka-0-8_2.11:2.1.0"
+
+os.environ["PYSPARK_SUBMIT_ARGS"] = ("--packages {},{} pyspark-shell".format(kafka, spark))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--topic', type=str, default='Product')
+
+args = parser.parse_args()
+
+
+
+if __name__ == '__main__':
+
+    # init spark session
+    spark = SparkSession.\
+        builder.\
+        appName(f"Crawl-{args.topic}").\
+        master("spark://spark-master:7077").\
+        config("spark.executor.memory", "512m").\
+        getOrCreate()
+    
+
+    KAFKA_BROKER_1='kafka-1:9092'
+    KAFKA_BROKER_2='kafka-2:9092'
+    KAFKA_BROKER_3='kafka-3:9092'
+    
+    KAFKA_TOPIC= args.topic
+    kafkaMessages = spark \
+      .readStream \
+      .format("kafka") \
+      .option("kafka.bootstrap.servers", f"{KAFKA_BROKER_1},{KAFKA_BROKER_2},{KAFKA_BROKER_3}") \
+      .option("subscribe", KAFKA_TOPIC) \
+      .option("startingOffsets", "earliest") \
+      .load()
+    
+    message = kafkaMessages.selectExpr("CAST(value AS STRING)")
+
+    fileStream = message.writeStream \
+                        .trigger(processingTime='10 seconds')\
+                        .queryName("Persist the processed data") \
+                        .outputMode("append") \
+                        .format("parquet") \
+                        .option("path", f"hdfs://namenode:9000/demo/{KAFKA_TOPIC}") \
+                        .option("failOnDataLoss","false") \
+                        .option("checkpointLocation", f"hdfs://namenode:9000/demo/checkpoints_{KAFKA_TOPIC}") \
+                        .start()
+    while True:
+        time.sleep(5)
+        if fileStream.lastProgress != None:
+            if fileStream.lastProgress['numInputRows'] == 0:
+                print('No data inseart in Kafka -> Stop spark...')
+                fileStream.stop()
+                spark.stop()
+                break
+            else:
+                print('Last procesing item:', fileStream.lastProgress['numInputRows'])
+                time.sleep(1)
+                
+                
---
+++ code/demo/hdfsInsertToElas.py
@@ -0,0 +1,51 @@
+from json import loads
+from pyspark.sql import SparkSession
+import warnings
+warnings.filterwarnings("ignore")
+from pyspark.sql.functions import col,from_json,udf,split,explode
+from pyspark.ml.feature import NGram
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType,MapType,FloatType,ArrayType
+
+import os 
+es = "org.elasticsearch:elasticsearch-spark-30_2.12:7.12.1"
+http_scala='org.apache.httpcomponents:httpclient:4.5.13'
+common_client='commons-httpclient:commons-httpclient:3.1'
+
+os.environ["PYSPARK_SUBMIT_ARGS"] = ("--packages {},{},{} pyspark-shell".format(es, http_scala,common_client))
+
+
+
+spark = SparkSession.\
+        builder.\
+        appName("hdfsToElasticsearch").\
+        master("spark://spark-master:7077").\
+        config("spark.executor.memory", "1024m").\
+        getOrCreate()
+
+
+list_item = ['Product_Shop']
+
+
+
+hdfs_url = 'hdfs://namenode:9000/analysis/{}'
+
+for item in list_item:
+    print(f'Start reading {item}...')
+    data_path = hdfs_url.format(item)
+    df_data = spark.read.parquet(data_path).limit(100)
+    
+    index_name = f'demo_{item.lower()}'
+    
+    df_data.write.format(
+            "org.elasticsearch.spark.sql"
+        ).option(
+            "es.resource", '%s/%s' % (index_name, 'data')
+        ).option(
+            "es.nodes", 'es01'
+        ).option(
+            "es.port", 9200
+        ).save()
+
+    print(f'Inserted {item} data into Elasticsearch...')
+
+spark.stop()
